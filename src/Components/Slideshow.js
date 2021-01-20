ab2db7569058075146856267e7c3e3d80b4458e0
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-01-20 17:56:55 -0500

    working on slideshow

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -11,25 +11,48 @@ const Slideshow = () => {
 
     const { docs } = useFirestore('slideshowImages')
 
+    let slideIndex = 0;
+
     const firebaseImages = (url) => {
         let img = document.getElementById("myimg");
         img.src = url;
     };
 
-    let i = 0;
-
-    if (docs.length === 8) {
-        firebaseImages(docs[i].url);
-    } else { console.log("Slideshow Function Error...") };
-
-    const slideshowImageChanger = (n) => {
-        i += n;
-        if (i > 7) {
-            i = 0
-        } else if (i < 0) {
-            i = 7
+    const showSlides = () => {
+        if (docs.length === 8) {
+            firebaseImages(docs[slideIndex].url);
         };
     }
+    showSlides();
+
+    const slideshowImageChanger = async (n) => {
+        slideIndex += n;
+        if (slideIndex > 7) {
+            slideIndex = 0
+        } else if (slideIndex < 0) {
+            slideIndex = 7
+        };
+        showSlides();
+        // await clearInterval();
+        // await setInterval(autoSlideshowImageChanger, 5000);
+    }
+
+    //Automatic Slideshow (5 second timer, adds 1 to the slideIndex per 5 seconds)
+    const autoSlideshowImageChanger = () => {
+        slideshowImageChanger(1);
+        console.log("image changed")
+    };
+
+    let auto = setInterval(autoSlideshowImageChanger, 5000);
+
+
+    // //Stopping Auto-Slideshow
+    // const stopAuto = () => {
+    //     clearInterval(auto)
+    // };
+
+
+
 
     /////////////Automatic Slideshow/////////////////////
     // const slideshowImageChanger = () => {
@@ -47,24 +70,6 @@ const Slideshow = () => {
     //         slideshowImageChanger();
     //     } else { console.log("Slideshow Function Error...") };
     // }, 4000)
-
-
-    // setTimeout(
-    //     () => {
-
-    // , 4000);
-
-    // if (docs.length === 8) {
-    //     for (let i = 0; i < docs.length; i++) {
-    //         setTimeout(() => {
-    //             firebaseImages(docs[i].url);
-    //         }, 4000);
-
-    //         if (i === 7) {
-    //             i = 0;
-    //         } else (console.log("Slideshow function messed up..."));
-    //     }
-    // } else (console.log("Slideshow Function Error..."));
 
 
     return (
