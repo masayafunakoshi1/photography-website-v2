cbfa2abe90cf6c166035c92f8ed86ba49575a4a6
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-01-21 23:34:04 -0500

    Added image grid, modified some homepage css

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
-import MenuBar from './MenuBar'
 
 const Contact = () => {
     return (
         <div>
-            <MenuBar />
             <div>
                 <form noValidate autoComplete="off">
                     <TextField id="standard-basic" label="Full-Name" />
