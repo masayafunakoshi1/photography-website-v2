e0e7ea4952f1f218b5e1a8ab08e9fef6d01c886a
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-01-25 16:47:53 -0500

    Attempting to create smoother UI with menu and slideshow. Added effect to grid items.

---
+++ src/Components/FadeOutMenu.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react'
+
+const FadeOutMenu = (props) => {
+    const [menuScroll, setMenuScroll] = useState(0);
+    const domRef = React.useRef();
+
+
+    ////////////////Attempts to use Intersection Observer/////////////
+    
+
+    // useEffect(() => {
+    //     const observer = new IntersectionObserver(entries => {
+    //         entries.forEach(entry => setMenuScroll(entry.isIntersecting))
+    //         console.log(entries)
+    //     });
+    //     observer.observe(domRef.current)
+    // }, []);
+        
+
+    // const io_options = {
+    // root: document.body,
+    // rootMargin: '0px 0px -100% 0px',
+    // threshold: 0
+    // };
+    // const io_observer = new IntersectionObserver(io_callback, io_options);
+
+    // domRef.forEach(element => {
+    // io_observer.observe(element);
+    // });
+
+    // function io_callback (entries, observer) {
+    // entries.forEach(entry => {
+    //     entry.target.classList.toggle(entry.isIntersecting);
+    // });
+    // }
+
+    ////////Scroll Listener////////////
+
+
+
+
+    return (
+        <div className={`menu ${menuScroll ? 'active' : ''}`}
+        ref={domRef}>
+            
+            {props.children}
+        </div>
+    )
+}
+
+export default FadeOutMenu
