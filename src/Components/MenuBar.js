ff47111a5a4c16d38a731408da688eae68467485
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-01-26 22:31:00 -0500

    Got React Router working, but problem with slideshow API call

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -2,11 +2,8 @@ import React, { useState } from 'react'
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import {
-    BrowserRouter as Router,
-    Link,
-    Route,
+    NavLink,
 } from "react-router-dom";
-import Contact from './Contact'
 
 
 const MenuBar = () => {
@@ -22,41 +19,35 @@ const MenuBar = () => {
 
     return (
         <div className="menuApp">
-            <Router>
-                <header className="menu">
-                    <h1 className="title"><Link to="/">MasaFuna <br />Photos</Link></h1>
+            <header className="menu">
+                <h1 className="title"><NavLink to="/">MasaFuna <br />Photos </NavLink></h1>
 
-                    <ul className="menu-items">
-                        <li>
-                            <a
-                                aria-controls="simple-menu"
-                                aria-haspopup="true"
-                                onClick={handleHover}
-                            >
-                                Portolio&nbsp;&#8595;
-                            </a>
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={anchorEl}
-                                keepMounted
-                                open={Boolean(anchorEl)}
-                            >
+                <ul className="menu-items">
+                    <li>
+                        <a
+                            aria-controls="simple-menu"
+                            aria-haspopup="true"
+                            onClick={handleHover}
+                        >
+                            Portolio&nbsp;&#8595;
+                        </a>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                        >
 
-                                <MenuItem onClick={handleClose}>Weddings</MenuItem>
-                                <MenuItem onClick={handleClose}>Events</MenuItem>
-                                <MenuItem onClick={handleClose}>Nature</MenuItem>
-                            </Menu>
-                        </li>
-                        <li><a >About</a></li>
-                        <li><a >FAQ</a></li>
-                        <li><Link to="/Contact">Contact</Link></li>
-                    </ul>
-                </header>
-
-            
-                <Route path="/Contact" component={Contact}></Route>
-
-            </Router>
+                            <MenuItem onClick={handleClose}>Weddings</MenuItem>
+                            <MenuItem onClick={handleClose}>Events</MenuItem>
+                            <MenuItem onClick={handleClose}>Nature</MenuItem>
+                        </Menu>
+                    </li>
+                    <li><a >About</a></li>
+                    <li><a >FAQ</a></li>
+                    <li><NavLink to="/Contact">Contact</NavLink></li>
+                </ul>
+            </header>
         </div >
     )
 }
