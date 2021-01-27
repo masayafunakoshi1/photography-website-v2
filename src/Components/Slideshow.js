40810346bf3375dd2a7ba066e57787ff27a0b04b
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-01-27 17:39:31 -0500

    In process of firguring out 2 slideshow issues. When on different route, slideshow.js function still runs causing issues. Also need to figure out image transitions.

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -8,25 +8,25 @@ import useFirestore from '../hooks/useFirestore'
 const Slideshow = () => {
 
      useEffect(() => {
-        showSlides()
+        showSlides();
     }, []);
 
     //Using firebase API storage, calling a google cloud URL and turning it into a regular URL and download it into the src of <img id="myimg">
 
     const { docs } = useFirestore('slideshowImages')
-
     let slideIndex = 0;
+
+
+    const showSlides = () => {
+    if (docs.length === 8) {
+        firebaseImages(docs[slideIndex].url);
+        };
+    }
 
     const firebaseImages = (url) => {
         let img = document.getElementById("myimg");
         img.src = url;
     };
-
-    const showSlides = () => {
-        if (docs.length === 8) {
-            firebaseImages(docs[slideIndex].url);
-        };
-    }
 
     const slideshowImageChanger = async (n) => {
         slideIndex += n;
@@ -43,11 +43,19 @@ const Slideshow = () => {
     //Automatic Slideshow (5 second timer, adds 1 to the slideIndex per 5 seconds)
     const autoSlideshowImageChanger = () => {
         slideshowImageChanger(1);
-        console.log("image changed")
-    };
+        console.log("image changed");
+        setTimeout(autoSlideshowImageChanger, 5000)
+    }
+        setTimeout(autoSlideshowImageChanger, 100)
 
-    let auto = setInterval(autoSlideshowImageChanger, 5000);
+    const slideshowImg = async (e) => {
+        e.target.className = "slideshowImg";
+    }
 
+    // setTimeout(function autoSlideshowImageChanger() {
+    //     slideshowImageChanger(1);
+    //     setTimeout(autoSlideshowImageChanger, 5000);
+    //     }, 5000);
 
     // //Stopping Auto-Slideshow
     // const stopAuto = () => {
@@ -69,13 +77,13 @@ const Slideshow = () => {
         <div className="slideshowSegment">
             <Container className="slideshow" className={classes.container}>
                 <Fab onClick={() => { slideshowImageChanger(1) }} className="rightArrow"><span>&#10095;</span></Fab>
-                    <img id="myimg" className="slideshowImg"/>
+                    <img id="myimg" onChange={(e) => slideshowImg(e)}/>
                 <Fab onClick={() => { slideshowImageChanger(-1) }} className="leftArrow"><span>&#10094;</span></Fab>
             </Container>
 
-                <Container className="quote">
-                    <span>Photography</span> is a medium <br /> which allows you to experience life <br /> from another's perspective...
-                </Container>
+            <Container className="quote">
+                <span>Photography</span> is a medium <br /> which allows you to experience life <br /> from another's perspective...
+            </Container>
         </div>
     )
 }
