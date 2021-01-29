810692e20456c58360d232f5d6dbf5607ab6e779
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-01-28 22:00:05 -0500

    Still trying to figure out slideshow problems from last commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TextField from '@material-ui/core/TextField';
 
 const Contact = () => {
+    
     return (
         <div>
             <div>
