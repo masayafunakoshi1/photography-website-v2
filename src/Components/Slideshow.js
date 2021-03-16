4006f11a3090aaa15913124b2c0401bf4e47d0bf
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-03-15 23:43:28 -0400

    Began gallery pages, back-to-top button need work.

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -2,93 +2,27 @@ import React, {useState, useEffect} from 'react'
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
-import useFirestore from '../hooks/useFirestore'
 import Loading from './Loading';
 
 
 const Slideshow = () => {
-    //Using firebase API storage, calling a google cloud URL and turning it into a regular URL and download it into the src of <img id="myimg">
-    // const { docs } = useFirestore('slideshowImages');
     const [slideIndex, setSlideIndex] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
-
-    //Slideshow Images from Public Folder
-    const urlData = [
-    {
-        title: 'Shiba-Dog',
-        description: 'Close up picture of a Shiba Inu (Shiba Dog) in Japan.',
-        imageUrl: 'images/slideshowImgs/IMG_0600.jpg',
-    },
-    {
-        title: 'Tree-of-Blessing',
-        description: 'Picture of a Japanese Maple tree during a foggy morning sunrise.',
-        imageUrl: 'images/slideshowImgs/IMG_1430-2.jpg',
-    },
-    {
-        title: 'Chipmunk',
-        description: 'Photo of a wild chipmunk, cheeks stuffed with birdfeed.',
-        imageUrl: 'images/slideshowImgs/IMG_3836.jpg',
-    },
-    {
-        title: 'Joanna-Kushal-Engagement-Music',
-        description: 'A beautiful musical performance for an even more beautiful engagement ceremony.',
-        imageUrl: 'images/slideshowImgs/IMG_6669.jpg',
-    },
-    {
-        title: 'Peace-Road-Prayer',
-        description: 'During an event called Peace Road, a group of both young and old, promoting peace across the country.',
-        imageUrl: 'images/slideshowImgs/IMG_6726.jpg',
-    },
-    {
-        title: 'Paki-Wedding-Photo',
-        description: 'A newly-wed couple taking wedding photos on the beautiful Belvedere property.',
-        imageUrl: 'images/slideshowImgs/IMG_7114.jpg',
-    },
-    {
-        title: 'Kevin-Patricia-AisleWalk',
-        description: 'The bride and her father walk down the aisle on her special day.',
-        imageUrl: 'images/slideshowImgs/IMG_7544.jpg',
-    },
-     {
-        title: 'Misty-Mountain-Heron',
-        description: 'On a misty morning at a temple in Miyajima, Japan.',
-        imageUrl: 'images/slideshowImgs/IMG_9375.jpg',
-    }
-]
 
     const docs = urlData.map(currentImg => {
         return currentImg.imageUrl;
         // return process.env.PUBLIC_URL + currentImg.imageUrl
     })
 
-    //Show image from Firestore API after if() is fulfilled
-    // const showSlides = () => {
-    //     if(docs.length === 8){
-    //         firebaseImages(docs[slideIndex].url);
-    //         console.log("Loaded length 8");
-    //         } else {
-    //             console.log('error')
-    //         }
-    //     imageFadeIn();
-    // }
-
      const showSlides = () => {
         document.getElementById('myimg').src = docs[slideIndex] ;
         imageFadeIn();
         setIsLoading(false);
      }
-
-    // const firebaseImages = (url) => {
-    //     document.getElementById("myimg").src = url;
-    //     console.log("image changed showed")
-    //     setIsLoading(false)
-    // };
-
     
      //Allows user to manually change the slideshow with arrows, also lets slideshow to change automatically with settimeout function call
     const slideshowImageChanger = (n) => {
         setSlideIndex(slideIndex + n)
-        console.log(docs)
         showSlides(); 
     }
 
@@ -150,19 +84,6 @@ const Slideshow = () => {
         return() => clearTimeout(timer)
     }, [showSlides])
 
-
-       //Styles Material UI Slideshow
-    const useStyles = makeStyles(() => ({
-        container: {
-        maxWidth: 1300,
-        alignItems: "right",
-        justifyContent: "right",
-            },
-        }
-    ));
-
-    const classes = useStyles();
-
     return (
         <div className="slideshowSegment">
             <Container className="slideshow" >
@@ -180,3 +101,48 @@ const Slideshow = () => {
 }
 
 export default Slideshow
+
+
+ //Slideshow Images from Public Folder
+const urlData = [
+    {
+        title: 'Shiba-Dog',
+        description: 'Close up picture of a Shiba Inu (Shiba Dog) in Japan.',
+        imageUrl: 'images/slideshowImgs/IMG_0600.jpg',
+    },
+    {
+        title: 'Tree-of-Blessing',
+        description: 'Picture of a Japanese Maple tree during a foggy morning sunrise.',
+        imageUrl: 'images/slideshowImgs/IMG_1430-2.jpg',
+    },
+    {
+        title: 'Chipmunk',
+        description: 'Photo of a wild chipmunk, cheeks stuffed with birdfeed.',
+        imageUrl: 'images/slideshowImgs/IMG_3836.jpg',
+    },
+    {
+        title: 'Joanna-Kushal-Engagement-Music',
+        description: 'A beautiful musical performance for an even more beautiful engagement ceremony.',
+        imageUrl: 'images/slideshowImgs/IMG_6669.jpg',
+    },
+    {
+        title: 'Peace-Road-Prayer',
+        description: 'During an event called Peace Road, a group of both young and old, promoting peace across the country.',
+        imageUrl: 'images/slideshowImgs/IMG_6726.jpg',
+    },
+    {
+        title: 'Paki-Wedding-Photo',
+        description: 'A newly-wed couple taking wedding photos on the beautiful Belvedere property.',
+        imageUrl: 'images/slideshowImgs/IMG_7114.jpg',
+    },
+    {
+        title: 'Kevin-Patricia-AisleWalk',
+        description: 'The bride and her father walk down the aisle on her special day.',
+        imageUrl: 'images/slideshowImgs/IMG_7544.jpg',
+    },
+     {
+        title: 'Misty-Mountain-Heron',
+        description: 'On a misty morning at a temple in Miyajima, Japan.',
+        imageUrl: 'images/slideshowImgs/IMG_9375.jpg',
+    }
+]
--- src/Components/galleries/Nature.js
+++ src/Components/galleries/Nature.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const Nature = () => {
     return (
         <div>
-            
+            <h1>Nature</h1>
         </div>
     )
 }
