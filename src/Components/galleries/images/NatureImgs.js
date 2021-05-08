242938bf8d76e134e1b0f520376b060479089147
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-05-08 18:24:44 -0400

    Resized most imgs, still need to do a little more

--- src/Components/galleries/images/NatureImgs.js
+++ src/Components/galleries/images/NatureImgs.js
@@ -2,131 +2,124 @@ export default [
     {
         title: 'DeerPicture',
         description: 'Picture of a deer in the fall.',
-        imageUrl: 'images/miscImgs/Deer-min.jpg',
+        imageUrl: 'images/natureImgs/Deer-min.jpg',
     },
         {
         title: 'Hiking-Sunset',
         description: 'Toyo watching sunset after a long hike.',
-        imageUrl: 'images/miscImgs/IMG_0335-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_0335-min.jpg',
     },
         {
         title: 'shibaDog1',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_0600-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_0600-min.jpg',
     },
         {
         title: 'shibaDog2',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_0603-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_0603-min.jpg',
     },
         {
         title: 'TreeOfBlessing',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_1430-2-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_1430-2-min.jpg',
     },
-       
         {
         title: 'Chipmunk',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3795-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3795-min-min.jpg',
     },
         {
         title: 'Chipmunk2',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3805-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3805-min-min.jpg',
     },
         {
         title: 'Chipmunk3',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3808-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3808-min-min.jpg',
     },
         {
         title: 'Chipmunk4',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3811-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3811-min-min.jpg',
     },
         {
         title: 'Chipmunk5',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3820-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3820-min-min.jpg',
     },
         {
         title: 'Chipmunk6',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3822-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3822-min-min.jpg',
     },
         {
         title: 'Chipmunk7',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3825-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3825-min-min.jpg',
     },
         {
         title: 'Chipmunk8',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3863-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3863-min-min.jpg',
     },
         {
         title: 'Chipmunk9',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3887-min-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3887-min-min.jpg',
     },
             {
         title: 'GeeseSleeping',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_4271-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_4271-min.jpg',
     },
         {
         title: 'MissionImpossibleFrog',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_5997-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_5997-min.jpg',
     },
             {
         title: 'GrassyScene',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_4182-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_4182-min.jpg',
     },
         
         {
         title: 'BirdOnBranch',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_3996-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_3996-min.jpg',
     },
-       
             {
         title: 'Aquarium',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_8075-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_8075-min.jpg',
     },
             {
         title: 'Hawk',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_8681-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_8681-min.jpg',
     },
             {
         title: 'HeronOnJapaneseroof',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_9375-min.jpg',
-    },
-            {
-        title: 'SunsetHair',
-        description: '',
-        imageUrl: 'images/miscImgs/IMG_9562-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_9375-min.jpg',
     },
       {
         title: 'Philly',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_9641-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_9641-min.jpg',
     },
          
           {
         title: 'Philly2',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_9664-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_9664-min.jpg',
     },
           {
         title: 'Philly3',
         description: '',
-        imageUrl: 'images/miscImgs/IMG_9683-min.jpg',
+        imageUrl: 'images/natureImgs/IMG_9683-min.jpg',
     },
     
 ]
